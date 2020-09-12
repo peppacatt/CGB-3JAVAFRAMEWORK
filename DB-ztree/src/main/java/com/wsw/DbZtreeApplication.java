c445d4913959a0d1cd93394e4688d899da36a1cf
peppacatt <peppacatt@163.com>
2020-09-12 15:43:53 +0800

    ztree

---
+++ DB-ztree/src/main/java/com/wsw/DbZtreeApplication.java
@@ -0,0 +1,13 @@
+package com.wsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbZtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbZtreeApplication.class, args);
+	}
+
+}
