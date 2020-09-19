b36e4b537b76e769a352135d6d55311a8e0a089b
peppacatt <peppacatt@163.com>
2020-09-19 10:15:04 +0800

    v0919_1003

---
+++ db-sys-v0919_1003/src/main/java/com/cy/DbSysV09191003Application.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSysV09191003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSysV09191003Application.class, args);
+    }
+
+}
