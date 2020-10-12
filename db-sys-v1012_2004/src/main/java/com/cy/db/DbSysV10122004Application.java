65bed00f338c7303581d8b7e971e8c584d3b86d1
peppacatt <peppacatt@163.com>
2020-10-12 20:09:47 +0800

    creat

---
+++ db-sys-v1012_2004/src/main/java/com/cy/db/DbSysV10122004Application.java
@@ -0,0 +1,13 @@
+package com.cy.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSysV10122004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSysV10122004Application.class, args);
+    }
+
+}
---
+++ db-sys-v1012_2004/src/test/java/com/cy/db/DbSysV10122004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSysV10122004ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
