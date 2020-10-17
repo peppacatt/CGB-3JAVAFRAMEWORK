3ae084c756a77420109c931aa03698f13ed42696
peppacatt <peppacatt@163.com>
2020-10-17 17:38:56 +0800

    ex

--- db-sys-v1012_2004/src/main/java/com/cy/db/sys/service/impl/SysLogServiceImpl.java
+++ db-sys-v1012_2004/src/main/java/com/cy/db/sys/service/impl/SysLogServiceImpl.java
@@ -5,11 +5,13 @@ import com.cy.db.common.exception.ServiceException;
 import com.cy.db.sys.dao.SysLogDao;
 import com.cy.db.common.po.SysLog;
 import com.cy.db.sys.service.SysLogService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class SysLogServiceImpl implements SysLogService {
 
@@ -18,12 +20,15 @@ public class SysLogServiceImpl implements SysLogService {
 
     @Override
     public PageObject<SysLog> findPageObject(String username, Long pageCurrent) {
+        long t1 = System.currentTimeMillis();
         if(pageCurrent == null || pageCurrent < 1) throw new IllegalArgumentException("当前页码值无效");
         long rowCount = sysLogDao.findRowCount(username);
         if(rowCount ==0) throw new ServiceException("没有找到对应结果");
         int pageSize = 5;
         long startIndex = (pageCurrent-1)*pageSize;
         List<SysLog> records = sysLogDao.findPageCurrentRecords(username, startIndex, pageSize);
+        long t2 = System.currentTimeMillis();
+        log.info("time{}",t2-t1);
         return new PageObject<>(pageSize, records, pageCurrent, rowCount);
     }
 
